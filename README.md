26b1bd44a1b7817e83df3aad82c20ce1a760e344
Blinky <captinblinky@gmail.com>
2023-05-23 00:58:17 -0400

    HYUUUUGE updates

--- README.md
+++ README.md
@@ -11,5 +11,5 @@
  haha welll......
  - It can have **PERSONALITY**, thats right, you heard it here first, no longer will it be a stubborn bastard that only spits out garbage responses, it knows things now, its actually **BIG BRAIN**.
  - It can connect to different API's easier, given that I've programmed the whole thing in Python now instead of basically stealing [Desinc's Sally Bot](https://github.com/CheesiestRats/SallyBot), there is literally endless possibilities for connecting your application to any service that has a python module.
- -  Fill more stuff in here.
+ -  It can also dynamically interact with elements in the HTML page that is the Gradio WebUI which also means that it has all the fine control a standard user has.
 
--- program.py
+++ program.py
@@ -1,12 +1,16 @@
 import os
 import time
-import asyncio
+
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
+import re
+
+import asyncio
 import discord
 from dotenv import load_dotenv
 
@@ -17,14 +21,21 @@ GUILD = os.getenv('DISCORD_GUILD')
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
+data_directory = os.path.join(os.getcwd(), 'chrome-data')  # Set the custom data directory location
+
+chrome_options = Options()
+chrome_options.add_argument(f"--user-data-dir={data_directory}")  # Specify the custom data directory
+
 # Using Chrome to access the web
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=chrome_options)
 driver.get('http://127.0.0.1:7862')
 
 wait = WebDriverWait(driver, 20)  # Increase the timeout to 20 seconds
 
 # Find the textarea element
-textbox = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'textarea[data-testid="textbox"]')))
+textbox = WebDriverWait(driver, 10).until(
+    EC.visibility_of_element_located((By.CSS_SELECTOR, 'textarea[data-testid="textbox"]'))
+)
 
 @client.event
 async def on_ready():
@@ -35,34 +46,42 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!generate'):
-        prompt = message.content[10:]
+    content = message.content.lower()
+
+    # Define the regex pattern to match the desired words.
+    pattern = r'\bmint-chan|minty|she|her\b'
+
+    # Check if the message contains any matches
+    if re.search(pattern, content):
+        # Process the message and generate a response.
+        prompt = message.content
 
         async with message.channel.typing():
             result = await asyncio.get_event_loop().run_in_executor(None, run, prompt)
 
         if result:
-            await message.channel.send(result)
+            await message.reply(result)
         else:
-            await message.channel.send("I'm sorry, I couldn't generate a response for that prompt.")
+            await message.reply("Oopsie Woopsie, Blinky's computer had a little fucky wucky UwU")
 
 def run(prompt):
-    textbox.clear() # Clear any existing text
+    textbox.clear()  # Clear any existing text
     textbox.send_keys(prompt)
-    textbox.send_keys(Keys.RETURN) # Simulate pressing the Enter key to submit the text.
+    textbox.send_keys(Keys.RETURN)  # Simulate pressing the Enter key to submit the text.
 
-    time.sleep(1) # Wait for 1 second.
+    time.sleep(1)  # Wait for 1 second.
 
     try:
         # Wait until div.prose.svelte-1ybaih5.min is no longer visible
         message_body_locator = (By.CSS_SELECTOR, 'div.prose.svelte-1ybaih5.min')
         wait.until_not(EC.visibility_of_element_located(message_body_locator))
-        
+
         # Retrieve the generated text from the first message-body div
-        message_body_element = driver.find_element(By.CSS_SELECTOR, 'div.prose.svelte-1ybaih5 div.chat div.message div.text div.message-body p')
+        message_body_element = driver.find_element(By.CSS_SELECTOR,
+                                                   'div.prose.svelte-1ybaih5 div.chat div.message div.text div.message-body p')
         generated_text = message_body_element.text
         return generated_text
-    
+
     except Exception as e:
         error_message = "An error occurred: " + str(e)
         return error_message
